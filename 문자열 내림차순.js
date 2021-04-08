51e89650a1c8a318debbdec68e53a4c34e5ce34b
Soomin Shin <ssoomin1@naver.com>
2021-04-08 15:25:42 +0900

    '문자열 내림차순' 문제 해결

--- 문자열 내림차순.js
+++ 문자열 내림차순.js
@@ -1,6 +1,23 @@
 'use strict';
-
+//문자를 큰 것부터 작은순으로 정렬 
+//영문 대소문자로만 구성, 소문자>대문자
 function solution(s){
     let answer='';
+    let newString=s.split('');
+    let temp;
+    for(let i=0;i<newString.length-1;i++){
+        for(let j=i+1;j<newString.length;j++){
+            if(newString[i]<newString[j]){
+                temp=newString[i];
+                newString[i]=newString[j];
+                newString[j]=temp;
+            }
+        }
+    }
+
+    answer=newString.join('');
     return answer;
 }
+
+//출력: gfedcbZ
+console.log(solution("Zbcdefg"));
