2318a2d6b99ab83d96f1fae8726439c5fed4af2f
Soomin Shin <ssoomin1@naver.com>
2021-04-27 09:18:22 +0900

    '체육복' 문제 해결

--- 체육복.js
+++ 체육복.js
@@ -1,37 +1,24 @@
-// function solution(n, lost, reserve) {
-//     //let answer = n.toString().split('').map(e=>parseInt(e,10));
-//     let answer=[]; //[1,2,3,4,5] [2,4]
-//     for(let i=1;i<=n;i++){
-//         answer.push(i);
-//     }
-//     let haveArr=[];
-//     // for(let i=0;i<answer.length;i++){
-//     //     for(let j=0;j<lost.length;j++){
-//     //         if(answer){
-//     //             haveArr.push(answer[i]);
-//     //         }
-//     //     }
-//     // }
-
-//     for(let i=0;i<lost.length;i++){
-//         if(answer.includes(lost[i])===true){
-//            answer.splice(lost[i],1);
-//         }
-//     }
-//     return answer;
-// }
-
-function solution(n, lost, reserve) {
+function solution(n,lost,reserve){
     let answer=[];
-    for(let i=1;i<=n;i++){
-        answer.push(i);
+    for(let i=0;i<n;i++){answer.push(1); } //기본적으로 모든 학생들에게 1개씩 부여
+    for(let i=0;i<answer.length;i++){
+        lost.includes(i+1)?answer[i]-=1:answer[i];
+        reserve.includes(i+1)?answer[i]+=1:answer[i];
     }
-    if()
-
-    return answer;
+    for(let i=0;i<answer.length;i++){
+        if(answer[i]===0 && answer[i-1]===2){
+            answer[i-1]-=1;
+            answer[i]+=1;
+        }else if(answer[i]===0 && answer[i+1]===2){
+            answer[i+1]-=1;
+            answer[i]+=1;
+        }
+    }
+    const canArr=answer.filter((e)=>e>=1);
+    return canArr.length;
 }
 
-//console.log(solution(5,[2,4],[1,3,5]))
+
 console.log(solution(5,[2,4],[1,3,5]));
 console.log(solution(5,[2,4],[3]));
 console.log(solution(3,[3],[1]));
