d5675696df845bbfce84c079691fcd3299b3fd4b
Soomin Shin <ssoomin1@naver.com>
2021-04-28 07:41:13 +0900

    '가장 큰 수' 문제 해결

---
+++ 가장_큰_수.js
@@ -0,0 +1,7 @@
+function solution(numbers){
+    let answer=numbers.map((e)=>e+'').sort((a,b)=>(b+a)-(a+b)).join('');
+    return answer[0]===0?'0':answer;
+}
+
+console.log(solution([6,10,2]));
+console.log(solution([3,30,34,5,9]));
